0fd1240c5c51d7a9897b754554bc4cb0e46ffce0
mailtoatanu <mailtoatanu@gmail.com>
2017-03-29 01:00:28 +0530

    Add files via upload

---
+++ 01. Dice Rolling Simulator.py
@@ -0,0 +1,39 @@
+#This is Dice Rolling Simulator
+#Asks user minimum and maximum value of the dice
+#Throws up a random number within the minimum and the maximum number
+
+import random
+
+gameon = 'yes'  #The game repeat controller
+
+
+print("Hello There! Welcome to an all new Dice Rolling Simulator!\n")  #Welcome statement. Does not add value to the code
+
+#try-except block is used to make the workable all the time
+try:
+    while gameon == 'yes':
+        print("What is going to be the minimum value of your dice?")
+        dice_min_val = int(input())
+        print("What is going to be the minimum value of your dice?")
+        dice_max_val = int(input())
+        dice_output = random.randint(dice_min_val,dice_max_val)
+        print("And here goes the result. The dice has rolled out to",dice_output)
+        print("Do you want to roll another dice?(y/n)")
+        user_input = input()
+        while user_input != 'y' and user_input != 'n':     #Check for valid input and ask for till a valid input is provided
+            print("Input not recognized. Please provie y/n as valid input.")
+            user_input = input()
+        if user_input == 'n':
+            gameon = "no"
+            
+    print("It was nice playing dice with you. Have a great day. Bye bye.")
+
+except:
+    print("Oop! Something went wront. Please try again.")
+    
+
+
+
+
+
+
